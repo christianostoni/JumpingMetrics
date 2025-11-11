c80df2d96cbab2c911730e8afb1126f607a8e652
christianostoni <christianostoni82@gmail.com>
2025-11-11 14:05:47 +0100

    Initial Commit

---
+++ jumping_time/jumping_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  jumping_timeApp.swift
+//  jumping_time
+//
+//  Created by Christian Ostoni on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct jumping_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
